f85070617119e2695f90e99d3050618caae1555c
mrjohnwick95 <darkwolfjefferson1995@gmail.com>
2025-11-09 20:57:35 -0500

    update host from railway

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, '/')));
 
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor escuchando en http://${HOST}:${PORT}`);
 });
